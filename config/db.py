3839937aa059cf36cbf02b25f69f33e5eb919658
Giovanni <giovanni.martinez.vivaldoipn@gmail.com>
2022-03-29 16:00:35 -0600

    final

--- config/db.py
+++ config/db.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 
-mongo_url = 'mongodb://mongo:iPAnHlBPYNyPlsoIltgc@containers-us-west-15.railway.app:7323'
+mongo_url = 'mongodb+srv://GiovanniVivaldo:fdWWmvqFsNjPXujP@cluster0.j3qff.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 conn = MongoClient(mongo_url)
